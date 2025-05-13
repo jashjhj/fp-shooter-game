1d33341b79fc74018084ded21db4a57fff4c636f
jas <jasmineeades7@gmail.com>
2025-05-13 16:01:30 +0100

    Initial - stable chaarcter scene for testing

---
+++ player/scripts/player.gd
@@ -0,0 +1,84 @@
+class_name Player extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+var camera_rot_x = 0; # initial values - set like this so can be set in editor
+var camera_rot_y = 0;
+
+var is_mouse_focused = true;
+
+@export_range(0, 180, 1.0, "degrees") var LOOK_MAX_VERT = 100;
+@export_range(-180, 0, 1.0, "degrees") var LOOK_MIN_VERT = -80
+
+
+@onready var HEAD = $Torso/Head
+@onready var TORSO = $Torso
+@onready var CAMERA = $Torso/Head/Camera3D
+
+var CAMERA_CAPTURED:bool = false;
+
+func _ready():
+	Globals.PLAYER = self;
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	camera_rot_x = rotation.y; # initial values - set like this so can be set in editor
+	camera_rot_y = rotation.x;
+
+##INPUT SCRIPT
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and is_mouse_focused:
+		#Handle mouse movement
+		
+		if(!CAMERA_CAPTURED): # if free-roaming
+			
+			camera_rot_x += event.relative.x * -Settings.MouseSensitivity.x
+			camera_rot_y += event.relative.y * -Settings.MouseSensitivity.y
+			
+			transform.basis = Basis();
+			
+			rotate_object_local(Vector3(0, 1, 0), camera_rot_x);
+			
+			camera_rot_y = min(camera_rot_y, deg_to_rad(LOOK_MAX_VERT)); # impose limits
+			camera_rot_y = max(camera_rot_y, deg_to_rad(LOOK_MIN_VERT))
+			
+			TORSO.basis = Basis();
+			TORSO.rotate_object_local(Vector3(1, 0, 0), camera_rot_y/2);
+			HEAD.basis = Basis();
+			HEAD.rotate_object_local(Vector3(1, 0, 0), camera_rot_y/2);
+			
+	
+	if event.is_action_pressed("interact_1"): # RMB
+		is_mouse_focused = !is_mouse_focused;
+		if(is_mouse_focused):Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+		else:Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	
+	
+	if event.is_action_pressed("exit"): # Kill program
+		get_tree().quit()
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):# and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED/8)
+		velocity.z = move_toward(velocity.z, 0, SPEED/8)
+
+	#velocity.y = 0
+	move_and_slide()
---
+++ scripts/singletons/settings.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var MouseSensitivity := Vector2(0.01, 0.01);
