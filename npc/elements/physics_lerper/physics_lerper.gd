18043c382cf9614911c0750570515ca06f2f00a0
jas <jasmineeades7@gmail.com>
2025-06-18 19:43:26 +0100

    Now allows for force compensation - e.g. countering gravity.

--- npc/elements/physics_lerper/physics_lerper.gd
+++ npc/elements/physics_lerper/physics_lerper.gd
@@ -4,6 +4,7 @@ class_name Physics_Lerper extends Node
 @export var AUTO_TRIGGER:bool = false;
 @export var AUTO_TRIGGER_NODE:Node3D;
 
+##Stable up to ~ 1000ms-2 acceleration
 @export var FORCE:float = 7.0;
 @export var RESERVE_FORCE:float = 20;
 ##Value for how much the object should bounce
@@ -28,12 +29,13 @@ func apply_forces(delta:float) -> void:
 	
 	#Identifiers
 	var mass:float = RIGIDBODY.mass;
+	#Velocity is local to the point on the rigidbody.
 	var velocity:Vector3 = RIGIDBODY.linear_velocity
 	var pos:Vector3 = RIGIDBODY.global_position
 	
 	
 	
-	var delta_pos = AUTO_TRIGGER_NODE.global_position - RIGIDBODY.global_position
+	var delta_pos = AUTO_TRIGGER_NODE.global_position - pos
 	if(delta_pos.length() < SLOP):
 		delta_pos = Vector3.ZERO # This works as delta_pos.normalised = (0,0,0)
 		
@@ -43,7 +45,7 @@ func apply_forces(delta:float) -> void:
 	# Using v^2 = u^2 + 2as  =>  u = sqrt(2as) (+v=0)
 	var maximum_velocity:float = sqrt(abs(2 * delta_pos.length() * max_deceleration)) 
 	maximum_velocity *= BOUNCINESS
-	var velocity_along_delta = RIGIDBODY.linear_velocity.dot(delta_pos.normalized()) * delta_pos.normalized()
+	var velocity_along_delta = velocity.dot(delta_pos.normalized()) * delta_pos.normalized()
 	
 	var force_to_apply:Vector3 = delta_pos.normalized() * FORCE
 	
@@ -64,19 +66,19 @@ func apply_forces(delta:float) -> void:
 	
 	
 	#Lost force caluclations to counter gravity - TODO REDO to consider unexpected DVs correctly
-	#var lost_impulse = last_force*delta - RIGIDBODY.mass*(RIGIDBODY.linear_velocity - last_velocity)
-	#var lost_force_mag = lost_impulse.length() / delta
-	#lost_force_mag = min(RESERVE_FORCE, lost_force_mag)
-	#var appliable_lost_force = lost_impulse.normalized() * lost_force_mag
+	var lost_impulse = last_force*delta - mass*(velocity - last_velocity)
+	var lost_force_mag = lost_impulse.length() / delta
+	lost_force_mag = min(RESERVE_FORCE, lost_force_mag)
+	var appliable_lost_force = lost_impulse.normalized() * lost_force_mag
 	
-	last_velocity = RIGIDBODY.linear_velocity;
-	last_force = force_to_apply;
-	
-	
+	last_velocity = velocity;
+	last_force = force_to_apply + appliable_lost_force;
 	
 	
 	
-	RIGIDBODY.apply_central_force(force_to_apply)
+	
+	
+	RIGIDBODY.apply_central_force(force_to_apply + appliable_lost_force)
 	
 
 
