a23838cc1ce16540dea1a7aceb3cea942af0dc88
jas <jasmineeades7@gmail.com>
2025-07-11 14:42:22 +0100

    implemented impulse propagation from the body to the legs

--- npc/test_npcs/legs/legbot_3001.gd
+++ npc/test_npcs/legs/legbot_3001.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var BODY:RigidBody3D
+@export var BODY_HITTABLE:Hit_Component;
 
 ##Legs should be direct children of BODY
 @export var LEG1:LegBotLeg;
@@ -30,6 +31,7 @@ func _ready() -> void:
 	PHYSLERP.FORCE = 10;
 	PHYSLERP.RESERVE_FORCE = 40;
 	
+	BODY_HITTABLE.on_hit.connect(body_hit)
 
 
 var first = true
@@ -52,7 +54,7 @@ func _physics_process(delta: float) -> void:
 		set_leg_target(LEG2)
 		set_leg_target(LEG3)
 	
-	apply_offbalance_force()
+	apply_offbalance_force(delta)
 	
 	first = false
 
@@ -65,6 +67,10 @@ func _physics_process(delta: float) -> void:
 	#LEG1.update_leg()
 	#LEG2.update_leg()
 	#LEG3.update_leg()
+
+func body_hit():
+	var impulse = BODY_HITTABLE.last_impulse
+	apply_dv_to_feet(impulse/BODY.mass)
 
 func set_leg_target(leg:LegBotLeg) -> void:
 	var target_pos = calculate_leg_target(leg)
@@ -118,7 +124,19 @@ func get_centre_of_stable_area(arr:Array[Vector3]) -> Vector3:
 	return Vector3.ZERO
 
 
-
+func pick_leg_to_move():
+	var legs:Array[LegBotLeg] = [LEG1, LEG2, LEG3]
+	var best_leg:int = 0;
+	var best_leg_score:float = -INF
+	
+	for i in range(0, len(legs)):
+		var leg_goal_delta = legs[i].TARGET.global_position - legs[i].FOOT.global_position
+		var score = leg_goal_delta.length() + leg_goal_delta.dot(get_point_velocity(legs[i].global_position - BODY.global_position))
+		if score > best_leg_score:
+			best_leg_score = score
+			best_leg = i
+	
+	#Best leg si the one in the worst position and needs moving next.
 
 
 ##S = start of line, D = delta. Considers X,Z. returns component lambda of 1 as x and 2 as y. |     Simple mathematical solver.
@@ -138,69 +156,22 @@ func get_intersection_components(s1:Vector3, s2:Vector3, d1:Vector3, d2:Vector3)
 	
 	return Vector2(lambda, mu)
 
-func apply_offbalance_force():
+func apply_offbalance_force(delta:float):
 	var stable_area := calculate_stable_area()
 	
 	var stable_centre := get_centre_of_stable_area(stable_area)
 	var com_global:Vector3 = BODY.global_position + BODY.global_basis * BODY.center_of_mass
-	var com_delta = BODY.center_of_mass - stable_centre
+	#var com_delta = BODY.center_of_mass - stable_centre
 	
 	#Calculate closest point of the stable area
-	var pivot_point:Vector3;
-	if(len(stable_area) == 0):
-		return
-	elif(len(stable_area) == 1):
-		pivot_point = stable_area[0]
-	else: # This function to calculate closest poitn can be optimised by only considering the edges connected to clsoest vertex. Will not have to consider 'other side'
-		var likeliest_pair:Array[int] = [-1, -1];
-		var likeliest_piv_pos:Vector3 = Vector3.ZERO;
-		
-		# For each line - if delta is perpendicular to a line.
-		for i in range(0, len(stable_area) - 1):
-			for j in range(i+1, len(stable_area)):
-				var line_delta = (stable_area[j] - stable_area[i]) * Vector3(1, 1, 1)
-				var line_normal = line_delta.cross(Vector3.UP) # direction not specified. could be in or out
-				
-				var components := get_intersection_components(stable_area[i], com_global, line_delta, line_normal);
-				
-				
-				if(components.x >= 0.0 and components.x <= 1.0): # lines intersect.
-					
-					var piv_pos = stable_area[i].lerp(stable_area[j], components.x)
-					
-					
-					
-					#If pivot pos is further out than COM pos, ie. COM is within the polygon
-					if(((stable_centre - piv_pos)*Vector3(1, 0, 1)).length_squared() > ((stable_centre - (com_global))*Vector3(1,0,1)).length_squared()):
-						continue # This point can be disregarded.
-					else:
-						#Check case where its on other side, so dot product would be negative indicating they are not on the same side of the stable_centre
-						if(((stable_centre - piv_pos)*Vector3(1, 0, 1)).dot((stable_centre - (com_global))*Vector3(1,0,1)) < 0):
-							continue
-						likeliest_pair = [i, j]
-						likeliest_piv_pos = piv_pos
-		
-		
-		if(likeliest_pair[0] == -1 or likeliest_pair[1] == -1): # No good line - get nearest point
-
-			var nearest_point:Vector3 = Vector3.INF
-			var nearest_distance_squared:float = INF
-			for i in range(0, len(stable_area)):
-				var point_dist_squared := (stable_area[i] - (com_global)).length_squared()
-				if(point_dist_squared < nearest_distance_squared): # New point is closer
-					nearest_point = stable_area[i]
-					nearest_distance_squared = point_dist_squared
-			
-			pivot_point = nearest_point
-		else:
-			pivot_point = likeliest_piv_pos # Use previous result from nearest line
+	var pivot_point:Vector3 = get_closest_stable_point_to(com_global)
 		
 		#Finally - ensure that COM is actually outside of the calculated nearest point on perimeter of polygon
 		
-		if(((pivot_point - stable_centre)*Vector3(1, 0, 1)).length_squared() >= ((com_global - stable_centre)*Vector3(1,0,1)).length_squared()):
-			# Within shape
-			
-			return
+	if(((pivot_point - stable_centre)*Vector3(1, 0, 1)).length_squared() >= ((com_global - stable_centre)*Vector3(1,0,1)).length_squared() \
+		or pivot_point == Vector3.INF): # INF means no stable point, cancel
+		
+		return
 		
 		#print("im unstable hahah")
 		#Debug.point(pivot_point + BODY.global_position, 1, Color.RED)
@@ -213,13 +184,98 @@ func apply_offbalance_force():
 	var moment_component:float = BODY.mass * ProjectSettings.get_setting("physics/3d/default_gravity") * sin(angle)
 	var moment_direction:Vector3 = (com_pivot_delta_xz.normalized() - (Vector3.UP / tan(angle))).normalized() # prommy this works
 	
-	DebugDraw3D.draw_line(BODY.global_position + Vector3.UP*0.3, BODY.global_position + Vector3.UP*0.3 + moment_direction * moment_component / 40)
+	#DebugDraw3D.draw_line(BODY.global_position + Vector3.UP*0.3, BODY.global_position + Vector3.UP*0.3 + moment_direction * moment_component / 40)
 	BODY.apply_central_force(moment_component * moment_direction)
+	
+	apply_dv_to_feet(moment_component * moment_direction / BODY.mass * delta)
 	
 	#This torqic application may be unnecessary and glitzy
 	var torque_direction := moment_direction.rotated(Vector3.UP, PI/2)
 	BODY.apply_torque(torque_direction * moment_component / 40)
+
+##Pos is global, returns global position of point on stable polygon.
+func get_closest_stable_point_to(pos:Vector3) -> Vector3:
+	var stable_area := calculate_stable_area()
+	var stable_centre := get_centre_of_stable_area(stable_area)
 	
+	
+	#Calculate closest point of the stable area
+	var pivot_point:Vector3;
+	if(len(stable_area) == 0):
+		
+		push_warning("attempted to calculate Closest-Stable-Point when there is no stable area at all.")
+		return Vector3.INF # Returns this as warning, failure
+		
+		
+	elif(len(stable_area) == 1):
+		pivot_point = stable_area[0]
+	else: # This function to calculate closest poitn can be optimised by only considering the edges connected to clsoest vertex. Will not have to consider 'other side'
+		var likeliest_pair:Array[int] = [-1, -1];
+		var likeliest_piv_pos:Vector3 = Vector3.ZERO;
+		
+		# For each line - if delta is perpendicular to a line.
+		for i in range(0, len(stable_area) - 1):
+			for j in range(i+1, len(stable_area)):
+				var line_delta = (stable_area[j] - stable_area[i]) * Vector3(1, 1, 1)
+				var line_normal = line_delta.cross(Vector3.UP) # direction not specified. could be in or out
+				
+				var components := get_intersection_components(stable_area[i], pos, line_delta, line_normal);
+				
+				
+				if(components.x >= 0.0 and components.x <= 1.0): # lines intersect.
+					
+					var piv_pos = stable_area[i].lerp(stable_area[j], components.x)
+					
+					
+					
+					#If pivot pos is further out than COM pos, ie. COM is within the polygon
+					if(((stable_centre - piv_pos)*Vector3(1, 0, 1)).length_squared() > ((stable_centre - (pos))*Vector3(1,0,1)).length_squared()):
+						continue # This point can be disregarded.
+					else:
+						#Check case where its on other side, so dot product would be negative indicating they are not on the same side of the stable_centre
+						if(((stable_centre - piv_pos)*Vector3(1, 0, 1)).dot((stable_centre - (pos))*Vector3(1,0,1)) < 0):
+							continue
+						likeliest_pair = [i, j]
+						likeliest_piv_pos = piv_pos
+		
+		
+		if(likeliest_pair[0] == -1 or likeliest_pair[1] == -1): # No good line - get nearest point
+
+			var nearest_point:Vector3 = Vector3.INF
+			var nearest_distance_squared:float = INF
+			for i in range(0, len(stable_area)):
+				var point_dist_squared := (stable_area[i] - (pos)).length_squared()
+				if(point_dist_squared < nearest_distance_squared): # New point is closer
+					nearest_point = stable_area[i]
+					nearest_distance_squared = point_dist_squared
+			
+			pivot_point = nearest_point
+		else:
+			pivot_point = likeliest_piv_pos # Use previous result from nearest line
+	return pivot_point
+
+
+##All global space, applies a delta velocity around the moment of stability to feet. Takes the DV @ com
+func apply_dv_to_feet(dv:Vector3):
+	var com_global := BODY.global_position + BODY.global_basis*BODY.center_of_mass
+	var pivot_pos:Vector3 = get_closest_stable_point_to(com_global + dv * 100)
+	var com_delta := com_global - pivot_pos
+	var pivot_axis:Vector3 = dv.cross(com_delta).normalized();
+	
+	var delta_dv:float = dv.dot(-pivot_axis.cross(com_delta)) / com_delta.length()
+	
+	
+	var legs:Array[LegBotLeg] = [LEG1, LEG2, LEG3]
+	for leg in legs:
+		var foot_pivot_delta := leg.FOOT.global_position - pivot_pos
+		var foot_dv_dir:Vector3 = foot_pivot_delta.cross(pivot_axis).normalized()
+		var foot_dv = delta_dv * foot_pivot_delta.length()
+		
+		leg.apply_foot_impulse(foot_dv * foot_dv_dir * leg.FOOT.mass)
+		
+		#print(foot_dv)
+	
+
 
 #Where pos = global offset from Body origin (irrespective of current rotation/basis).
 func get_point_velocity(pos:Vector3) -> Vector3:
