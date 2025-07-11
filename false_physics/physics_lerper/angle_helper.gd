6bb306f7f0e6c6baabe92f60d36029a7a59fb711
jas <jasmineeades7@gmail.com>
2025-07-11 14:57:15 +0100

    Added functionality for angle helper (helper methods)

--- false_physics/physics_lerper/angle_helper.gd
+++ false_physics/physics_lerper/angle_helper.gd
@@ -1,20 +1,48 @@
+@tool
+
 class_name Angular_Damper extends Generic6DOFJoint3D
 
-@export var STIFFNESS:float = 8.0;
-@export var DAMPING:float = 2.0;
+@export var STIFFNESS:float = 8.0:
+	set(v):
+		STIFFNESS = v
+		update_stiffness(v)
+@export var DAMPING:float = 2.0:
+	set(v):
+		DAMPING = v
+		update_damping(v)
+@export var enabled:bool = true:
+	set(v):
+		enabled = v
+		set_spring(v)
+@export var locked:bool = false:
+	set(v):
+		locked = v
+		set_locked(locked)
 
 func _ready() -> void:
-	set("angular_limit_x/enabled", false)
-	set("angular_spring_x/enabled", true)
-	set("angular_spring_x/damping", DAMPING)
-	set("angular_spring_x/stiffness", STIFFNESS)
+	update_damping(DAMPING)
+	update_stiffness(STIFFNESS)
+	set_spring(enabled)
+	set_locked(locked)
 
-	set("angular_limit_y/enabled", false)
-	set("angular_spring_y/enabled", true)
-	set("angular_spring_y/damping", DAMPING)
-	set("angular_spring_y/stiffness", STIFFNESS)
 
-	set("angular_limit_z/enabled", false)
-	set("angular_spring_z/enabled", true)
-	set("angular_spring_z/damping", DAMPING)
-	set("angular_spring_z/stiffness", STIFFNESS)
+
+func update_damping(d):
+	set("angular_spring_z/damping", d)
+	set("angular_spring_y/damping", d)
+	set("angular_spring_x/damping", d)
+
+func update_stiffness(s):
+	set("angular_spring_z/stiffness", s)
+	set("angular_spring_y/stiffness", s)
+	set("angular_spring_x/stiffness", s)
+
+func set_spring(s:bool):
+	set("angular_spring_z/enabled", s)
+	set("angular_spring_y/enabled", s)
+	set("angular_spring_x/enabled", s)
+
+func set_locked(l:bool):
+	set("angular_limit_x/enabled", l)
+	set("angular_limit_y/enabled", l)
+	set("angular_limit_z/enabled", l)
