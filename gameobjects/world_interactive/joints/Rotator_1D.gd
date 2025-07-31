0a08275dd0df5a37ded8e70686d2738458c84b74
jas <jasmineeades7@gmail.com>
2025-07-31 18:34:00 +0100

    Implemented limits on rotator 1d

--- gameobjects/world_interactive/joints/Rotator_1D.gd
+++ gameobjects/world_interactive/joints/Rotator_1D.gd
@@ -5,6 +5,10 @@ class_name Rotator_1D extends Node3D
 ##Maximum degrees/Second
 @export_range(0, 360, 0.1, "radians_as_degrees") var ROTATION_SPEED:float = 1.0;
 
+@export var ANGLE_LIMITS_ENABLED:bool = false
+@export_range(-180, 180, 1.0, "radians_as_degrees") var MIN_ANGLE = -PI/4; 
+@export_range(-180, 180, 1.0, "radians_as_degrees") var MAX_ANGLE = PI/4;
+
 ##Amount of slop allowed. If Zero, does the ebst it can without jittering.
 @export_range(0, 30, 0.1, "radians_as_degrees") var SLOP:float = 0.0;
 
@@ -13,6 +17,9 @@ class_name Rotator_1D extends Node3D
 
 @export var IS_ACTIVE:bool = true;
 @export var DEBUG_AXES:bool = false;
+
+
+var current_angle:float = 0.0;
 
 ##A vector representing which way this Rotator_1D should look towards (-z) || In GLOBAL Space
 var target:Vector3:
@@ -62,8 +69,18 @@ func _process(delta: float) -> void:
 	
 	#angle_to_turn = TURN_SPEED * delta
 	
-	rotate(basis.y, angle_to_turn)
+	if(ANGLE_LIMITS_ENABLED):
+		if(current_angle + angle_to_turn > MAX_ANGLE):
+			angle_to_turn = MAX_ANGLE - current_angle
+			current_angle = MAX_ANGLE
+		
+		elif(current_angle + angle_to_turn < MIN_ANGLE):
+			angle_to_turn = MIN_ANGLE - current_angle
+			current_angle = MIN_ANGLE
+		else:
+			current_angle += angle_to_turn
 	
+	rotate(basis.y, angle_to_turn)
 	spin(angle_to_turn)
 
 ##Called every frame _process()
