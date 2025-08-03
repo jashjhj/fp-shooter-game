e3c8e4a2ed22f9760d109d9abd30769d9d8e735d
jas <jasmineeades7@gmail.com>
2025-08-03 17:18:39 +0100

    made plastic a tool script, and renamed to pastel

---
+++ assets/textures/plastics/pastel_colour_mesh.gd
@@ -0,0 +1,32 @@
+@tool
+class_name Pastel_Colour_Mesh extends MeshInstance3D
+
+
+## float between 0 and 1
+@export var health:float = 1.0:
+	set(v):
+		health = v
+		for i in range(0,get_surface_override_material_count()):
+			get_surface_override_material(i).set_shader_parameter("life", health)
+
+@export var RANDOMISE_ON_RUNTIME:bool = true;
+
+@export var RERANDOMISE:bool = false:
+	set(v):
+		RERANDOMISE = false
+		randomise_colours()
+
+
+## randomises colour for editor and to
+func _ready() -> void:
+	
+	if(RANDOMISE_ON_RUNTIME): randomise_colours()
+
+func randomise_colours():
+	for i in range(0,get_surface_override_material_count()):
+		
+		var MAT:ShaderMaterial = load("res://assets/textures/plastics/pastel_colour.tres").duplicate()
+		
+		#MAT.resource_local_to_scene = true
+		MAT.set_shader_parameter("color_seed", randf())
+		set_surface_override_material(i, MAT)
---
+++ gameobjects/world_interactive/hit_effects/Hit_Plastic_Shade.gd
@@ -0,0 +1,12 @@
+extends Hit_Component
+
+@export var PASTEL:Pastel_Colour_Mesh
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super._ready()
+	pass # Replace with function body.
+
+func hit(damage:float) -> void:
+	super.hit(damage)
+	pass
