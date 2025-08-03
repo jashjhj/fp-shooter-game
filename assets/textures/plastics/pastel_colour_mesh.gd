593f4287833221d29989597afbcd2be9d44dcfb9
jas <jasmineeades7@gmail.com>
2025-08-03 17:33:14 +0100

    Added limits to pastel_colour_mesh

--- assets/textures/plastics/pastel_colour_mesh.gd
+++ assets/textures/plastics/pastel_colour_mesh.gd
@@ -5,7 +5,7 @@ class_name Pastel_Colour_Mesh extends MeshInstance3D
 ## float between 0 and 1
 @export_range(0.0, 1.0, 0.001) var health:float = 1.0:
 	set(v):
-		health = v
+		health = min(1.0, max(0.0, v))
 		for i in range(0,get_surface_override_material_count()):
 			get_surface_override_material(i).set_shader_parameter("life", health)
 
