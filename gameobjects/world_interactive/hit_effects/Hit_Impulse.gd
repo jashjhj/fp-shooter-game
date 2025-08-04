ed0360ca440c027a372df0388e89c7355b66915a
jas <jasmineeades7@gmail.com>
2025-08-04 23:35:07 +0100

    Added impusle multiplier to all Hit_Impulses

--- gameobjects/world_interactive/hit_effects/Hit_Impulse.gd
+++ gameobjects/world_interactive/hit_effects/Hit_Impulse.gd
@@ -2,6 +2,8 @@ class_name Hit_Impulse extends Hit_Component
 
 ##Can be Owning Hittable_RB or otherwise. Auto-sets to parent if it is Rigidbody3D
 @export var IMPULSE_TO:RigidBody3D;
+##Multiplier applied to all impulses passing through. Good for simulating suspension, for example.
+@export var IMPULSE_MULTIPLIER:float = 1.0;
 
 func _ready() -> void:
 	super._ready()
@@ -11,6 +13,8 @@ func _ready() -> void:
 
 
 func hit(damage:float):
+	super.hit(damage)
+	last_impulse *= IMPULSE_MULTIPLIER
 	if IMPULSE_TO == null: return
 	IMPULSE_TO.apply_impulse(last_impulse, last_impulse_pos - IMPULSE_TO.global_position)
 	
