82725bfea541665adcc9b47bf1094fea81073a4e
jas <jasmineeades7@gmail.com>
2025-08-06 18:20:01 +0100

    Made the @tool more responsive

--- assets/textures/pastels/pastel_colour_mesh.gd
+++ assets/textures/pastels/pastel_colour_mesh.gd
@@ -32,7 +32,7 @@ class_name Pastel_Colour_Mesh extends MeshInstance3D
 @export var HIGHLIGHT_COLOUR:Color = Color(0.3, 0.2, 0.1):
 	set(v):
 		if(!is_initialised): init()
-		HIGHLIGHT_COLOUR
+		HIGHLIGHT_COLOUR = v
 		for mat in get_all_materials():
 			mat.set_shader_parameter("emission_color", Vector3(HIGHLIGHT_COLOUR.r, HIGHLIGHT_COLOUR.g, HIGHLIGHT_COLOUR.b))
 			mat.next_pass.set_shader_parameter("emission_color", Vector3(HIGHLIGHT_COLOUR.r, HIGHLIGHT_COLOUR.g, HIGHLIGHT_COLOUR.b))
@@ -49,6 +49,8 @@ class_name Pastel_Colour_Mesh extends MeshInstance3D
 			else:
 				if(HIGHLIGHT_THROUGH_OBJECTS): mat.next_pass.set_shader_parameter("emission", 0)
 				else:mat.set_shader_parameter("emission", 0)
+			
+			
 
 
 ##Makes the highlight work through walls.
@@ -72,22 +74,17 @@ func _ready() -> void:
 	if(!is_initialised): init()
 
 func init():
+	is_initialised = true
 	for i in range(0,get_surface_override_material_count()):
 		
 		var mat:ShaderMaterial = load("res://assets/textures/pastels/pastel_colour.tres").duplicate()
 		mat.next_pass = load("res://assets/textures/pastels/highlight_shader.tres").duplicate()
 		set_surface_override_material(i, mat)
-		
-		if(HIGHLIGHT_THROUGH_OBJECTS):
-			mat.next_pass.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
-			mat.set_shader_parameter("emission", 0)
-		else:
-			mat.next_pass.set_shader_parameter("emission", 0)
-			mat.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
 	
-	#HIGHLIGHT_THROUGH_OBJECTS = HIGHLIGHT_THROUGH_OBJECTS
-	#HIGHLIGHT_COLOUR = HIGHLIGHT_COLOUR
-	#HIGHLIGHT_STRENGTH = HIGHLIGHT_STRENGTH
+	#Calls getters to initialise
+	HIGHLIGHT_THROUGH_OBJECTS = HIGHLIGHT_THROUGH_OBJECTS
+	HIGHLIGHT_COLOUR = HIGHLIGHT_COLOUR
+	HIGHLIGHT_STRENGTH = HIGHLIGHT_STRENGTH
 	RERANDOMISE = true;
 
 func get_all_materials():
