1834292203be256de0af5fe4263b442a7c3928b5
jas <jasmineeades7@gmail.com>
2025-08-06 18:13:20 +0100

    Made shader work. Can now toggle whether its through walls or not

--- assets/textures/pastels/pastel_colour_mesh.gd
+++ assets/textures/pastels/pastel_colour_mesh.gd
@@ -1,4 +1,4 @@
-@tool
+
 class_name Pastel_Colour_Mesh extends MeshInstance3D
 
 
@@ -10,30 +10,88 @@ class_name Pastel_Colour_Mesh extends MeshInstance3D
 		for i in range(0,get_surface_override_material_count()):
 			get_surface_override_material(i).set_shader_parameter("life", health)
 
-@export var RANDOMISE_ON_RUNTIME:bool = true;
 
+##Button to rerandomise
 @export var RERANDOMISE:bool = false:
 	set(v):
 		RERANDOMISE = false
-		randomise_colours()
+		for mat in get_all_materials():
+			mat.set_shader_parameter("color_shade", randf())
 
-@export var EMISSIVE:float = 0.0:
+
+
+@export_group("Highlight", "HIGHLIGHT")
+@export var HIGHLIGHT_ENABLED:bool = false:
 	set(v):
-		EMISSIVE = v
-		randomise_colours()
+		if(!is_initialised): init()
+		
+		HIGHLIGHT_ENABLED = v
+		HIGHLIGHT_STRENGTH = HIGHLIGHT_STRENGTH # triggers setter
+		
+
+@export var HIGHLIGHT_COLOUR:Color = Color(0.3, 0.2, 0.1):
+	set(v):
+		if(!is_initialised): init()
+		HIGHLIGHT_COLOUR
+		for mat in get_all_materials():
+			mat.set_shader_parameter("emission_color", Vector3(HIGHLIGHT_COLOUR.r, HIGHLIGHT_COLOUR.g, HIGHLIGHT_COLOUR.b))
+			mat.next_pass.set_shader_parameter("emission_color", Vector3(HIGHLIGHT_COLOUR.r, HIGHLIGHT_COLOUR.g, HIGHLIGHT_COLOUR.b))
+
+@export var HIGHLIGHT_STRENGTH:float = 0.5:
+	set(v):
+		if(!is_initialised): init()
+		HIGHLIGHT_STRENGTH = v
+		for mat in get_all_materials():
+			if(HIGHLIGHT_ENABLED):
+				
+				if(HIGHLIGHT_THROUGH_OBJECTS): mat.next_pass.set_shader_parameter("emission", HIGHLIGHT_STRENGTH)
+				else:mat.set_shader_parameter("emission", HIGHLIGHT_STRENGTH)
+			else:
+				if(HIGHLIGHT_THROUGH_OBJECTS): mat.next_pass.set_shader_parameter("emission", 0)
+				else:mat.set_shader_parameter("emission", 0)
+
+
+##Makes the highlight work through walls.
+@export var HIGHLIGHT_THROUGH_OBJECTS:bool = false:
+	set(v):
+		HIGHLIGHT_THROUGH_OBJECTS = v;
+		for mat in get_all_materials():
+			if(HIGHLIGHT_THROUGH_OBJECTS):
+				mat.next_pass.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
+				mat.set_shader_parameter("emission", 0)
+			else:
+				mat.next_pass.set_shader_parameter("emission", 0)
+				mat.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
+
+
+var is_initialised:bool = false
 
 ## randomises colour for editor and to
 func _ready() -> void:
 	
-	if(RANDOMISE_ON_RUNTIME): randomise_colours()
+	if(!is_initialised): init()
 
-func randomise_colours():
+func init():
 	for i in range(0,get_surface_override_material_count()):
 		
-		var MAT:ShaderMaterial = load("res://assets/textures/pastels/pastel_colour.tres").duplicate()
+		var mat:ShaderMaterial = load("res://assets/textures/pastels/pastel_colour.tres").duplicate()
+		mat.next_pass = load("res://assets/textures/pastels/highlight_shader.tres").duplicate()
+		set_surface_override_material(i, mat)
 		
-		#MAT.resource_local_to_scene = true
-		MAT.set_shader_parameter("color_shade", randf())
-		MAT.next_pass = load("res://assets/textures/pastels/highlight_shader.tres").duplicate()
-		MAT.next_pass.set_shader_parameter("emission", EMISSIVE)
-		set_surface_override_material(i, MAT)
+		if(HIGHLIGHT_THROUGH_OBJECTS):
+			mat.next_pass.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
+			mat.set_shader_parameter("emission", 0)
+		else:
+			mat.next_pass.set_shader_parameter("emission", 0)
+			mat.set_shader_parameter("emission", HIGHLIGHT_STRENGTH if HIGHLIGHT_ENABLED else 0)
+	
+	#HIGHLIGHT_THROUGH_OBJECTS = HIGHLIGHT_THROUGH_OBJECTS
+	#HIGHLIGHT_COLOUR = HIGHLIGHT_COLOUR
+	#HIGHLIGHT_STRENGTH = HIGHLIGHT_STRENGTH
+	RERANDOMISE = true;
+
+func get_all_materials():
+	var out:Array[ShaderMaterial]
+	for i in range(0,get_surface_override_material_count()):
+		out.append(get_surface_override_material(i))
+	return out
