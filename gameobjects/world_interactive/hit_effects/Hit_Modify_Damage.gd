d8613e7650a204ec1fca67dea80216563cd1b850
jas <jasmineeades7@gmail.com>
2025-08-09 00:05:28 +0100

    Added Hit_modify_damage

---
+++ gameobjects/world_interactive/hit_effects/Hit_Modify_Damage.gd
@@ -0,0 +1,10 @@
+class_name Hit_Modify_Damage extends Hit_Component
+
+@export var HIT_CMP:Hit_Component;
+@export var DAMAGE_MULTIPLIER:float = 1.0;
+@export var IMPULSE_MULTIPLIER:float = 1.0;
+
+func hit(damage):
+	super(damage)
+	if(HIT_CMP == null): return
+	HIT_CMP.trigger(damage * DAMAGE_MULTIPLIER, last_impulse * IMPULSE_MULTIPLIER, last_impulse_pos)
