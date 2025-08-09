2d71743ce73d0d318532043d3df69f89ed340277
jas <jasmineeades7@gmail.com>
2025-08-09 11:32:33 +0100

    Added better collision to the totembot using NPC mask

--- gameobjects/world_interactive/hit_effects/Hittables/Hittable_RB.gd
+++ gameobjects/world_interactive/hit_effects/Hittables/Hittable_RB.gd
@@ -16,3 +16,17 @@ func _ready() -> void:
 	for child in get_children():
 		if(child is Hit_Component and !HIT_COMPONENTS.has(child)):
 			HIT_COMPONENTS.append(child)
+	
+	
+	
+	#contact_monitor = true
+	#max_contacts_reported = 3;
+#
+#func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	#for i in range(0, state.get_contact_count()):
+		#var impulse:Vector3 = state.get_contact_impulse(i) * (global_basis * state.get_contact_local_normal(i))
+		#if(impulse.length() > 1):
+			#
+			#for h in HIT_COMPONENTS:
+				#print(h.get_path())
+				#h.trigger(impulse.length(), Vector3.ZERO, global_position)
