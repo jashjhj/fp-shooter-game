f8fa3ebe986215beb903b0741845af5a9b3d1dad
jas <jasmineeades7@gmail.com>
2025-08-09 22:40:37 +0100

    1

---
+++ npc/enemy/totembot/stabilisation_centrifuge.gd
@@ -0,0 +1,41 @@
+class_name Stabilisation_Centrifuge extends Body_Segment
+
+@export var BODY:Grand_Body;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assert(len(IMPULSE_UPSTREAMS) != 0, "No impulse_Upstreams set on a Centrifuge")
+	super()
+	
+	IMPULSE_PROPOGATOR.DISABLE_UPSTREAM = true # overrides its behaviour
+	IMPULSE_PROPOGATOR.on_hit.connect(apply_random_impulse)
+	BODY.IS_CONTROLLED_COM_ACTIVE = true
+	BODY.CONTROLLED_COM = BODY.to_local(global_position)
+	
+	
+
+
+func apply_random_impulse():
+	var impulse = IMPULSE_PROPOGATOR.last_impulse;
+	
+	#Randomsie position of impulse grantedinto hemisphere out
+	impulse = impulse.rotated(global_basis.y, (randf()-0.5) * 2*PI)
+	impulse = impulse.rotated(global_basis.y.cross(impulse).normalized(), (randf()-0.5) * 2*PI)
+	
+	
+	BODY.apply_impulse(impulse * 20, global_position - BODY.global_position)
+	BODY.apply_torque(impulse * 2000)
+	
+	
+	#for i in IMPULSE_UPSTREAMS:
+		#i.trigger(IMPULSE_PROPOGATOR.last_damage, impulse, global_position)
+	
+
+
+func destroy():
+	
+	$Gib.trigger()
+	BODY.IS_CONTROLLED_COM_ACTIVE = false
+	
+	super.destroy()
