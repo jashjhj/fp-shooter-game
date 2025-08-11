90308e7d7578ee12cf04e0df71f24be136489082
jas <jasmineeades7@gmail.com>
2025-08-11 02:13:04 +0100

    Various lighting tweaks including sdfgi

--- maps/sandy_arena/trigger_summon_totembot.gd
+++ maps/sandy_arena/trigger_summon_totembot.gd
@@ -4,6 +4,6 @@ func trigger():
 	var new_bot = load("res://npc/enemy/totembot/totembot.tscn").instantiate()
 
 	new_bot.position = Vector3(randf_range(-10.0, 10.0), 15.0, randf_range(-10.0, 10.0)) # this gives errors but its kind of okay
-	get_tree().root.add_child(new_bot)
+	add_child(new_bot)
 	
 	#get_tree().root.add_child(new_bot)
